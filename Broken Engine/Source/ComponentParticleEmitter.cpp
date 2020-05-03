c549d107bb7ce35085d6eb9e775b646e6962df59
Joan Marín <36265613+X0KA@users.noreply.github.com>
2020-05-03 23:05:02 +0200

    Update ComponentParticleEmitter.cpp

--- Broken Engine/Source/ComponentParticleEmitter.cpp
+++ Broken Engine/Source/ComponentParticleEmitter.cpp
@@ -1165,6 +1165,7 @@ void ComponentParticleEmitter::Play()
 	emisionActive = true;
 	emisionStart = App->time->GetGameplayTimePassed() * 1000;
 	spawnClock = emisionStart;
+	firstEmision = true;
 }
 
 void ComponentParticleEmitter::Stop()

f0dda3f17b3d992a9436feb05a3e6f597ae80c62
Sergi PR <t3m1X@users.noreply.github.com>
2020-06-05 17:06:06 +0200

    Added preUpdate to ModuleTheading

--- Broken Engine/Source/ModuleThreading.h
+++ Broken Engine/Source/ModuleThreading.h
@@ -22,6 +22,7 @@ public:
 
 	bool Init(json& file) override;
 	bool Start() override;
+	update_status PreUpdate(float dt) override;
 	update_status Update(float dt) override;
 	update_status PostUpdate(float dt) override;
 	bool CleanUp() override;

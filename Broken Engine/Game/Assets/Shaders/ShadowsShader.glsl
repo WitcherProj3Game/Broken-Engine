aaf2d515ad26a3efb462bab39ed4b8431a0969c0
Aitor Simona Bouzas <simonaaitor@gmail.com>
2020-06-08 00:00:55 +0200

    adapted shadows shader, added geometry stage

--- Broken Engine/Game/Assets/Shaders/ShadowsShader.glsl
+++ Broken Engine/Game/Assets/Shaders/ShadowsShader.glsl
@@ -9,11 +9,11 @@ uniform mat4 u_Proj;
 uniform mat4 u_View;
 uniform mat4 u_Model;
 
-out vec2 v_texCoords;
+out vec2 vg_texCoords;
 
 void main()
 {
-	v_texCoords = a_TexCoord;
+	vg_texCoords = a_TexCoord;
 	gl_Position = u_Proj * u_View * u_Model * vec4(a_Position, 1.0);
 }
 
@@ -25,6 +25,13 @@ void main()
 uniform sampler2D u_AlbedoTexture;
 uniform int u_HasDiffuseTexture = 0;
 uniform vec4 u_Color = vec4(1.0);
+
+in vec4 FragPos;
+
+// FOR point shadows
+
+//uniform vec3 lightPos;
+//uniform float far_plane;
 
 in vec2 v_texCoords;
 
@@ -38,6 +45,49 @@ void main()
 
 	if(color.a < 0.01)
 		discard;
+
+
+	// FOR point shadows
+
+    // get distance between fragment and light source
+    //float lightDistance = length(FragPos.xyz - vec3(0,1,0));
+    
+    // map to [0;1] range by dividing by far_plane
+    //lightDistance = lightDistance / 20; // far_plane
+    
+    // write this as modified depth
+    //gl_FragDepth = lightDistance;
 }
 
 #endif
+
+#define GEOMETRY_SHADER
+#ifdef GEOMETRY_SHADER
+
+layout (triangles) in; 
+layout (triangle_strip, max_vertices = 18) out; 
+
+uniform mat4 shadowMatrices[6];
+
+in vec2 vg_texCoords[];
+out vec2 v_texCoords;
+
+out vec4 FragPos; // FragPos from GS (output per emitvertex)
+
+void main()
+{ 
+	for (int face = 0; face < 6; face++) 
+	{
+		gl_Layer = face; // built-in variable that specifies to which face we render.
+        for(int i = 0; i < 3; ++i) // for each triangle vertex
+        {
+			v_texCoords = vg_texCoords[face*3 + i];
+            FragPos = gl_in[i].gl_Position;
+            gl_Position = shadowMatrices[face] * FragPos;
+            EmitVertex();
+        }    
+        EndPrimitive();
+	}
+} 
+	
+#endif //GEOMETRY_SHADER
